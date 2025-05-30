415e8ba988cb29da7391b1e1cb70a1bc30daa515
Asmaa Taj El-Dein <asmaatajeldein@gmail.com>
2025-05-30 18:46:28 +0300

    refactor js-fundamentals day 2 task

--- 01-JS-Fundamentals/day-2/script.js
+++ 01-JS-Fundamentals/day-2/script.js
@@ -1,62 +1,62 @@
-// // Q1
+// Q1
 
-// for (var i = 10; i <= 20; i+= 2) {
-//     console.log(`Item ${i}`);
-// };
+for (var i = 10; i <= 20; i += 2) {
+    console.log(`Item ${i}`);
+}
 
-// // Q2
+// Q2
 
-// for (var i = 1; i <= 9; i+=2){
-//     console.log(i);
-// };
+for (var i = 1; i <= 9; i += 2) {
+    console.log(i);
+}
 
-// // Q3
+// Q3
 
-// alert("Welcome to my site");
-// do {
-//     var userName = prompt("Enter your name:");
-// } while(!isNaN(userName) || userName === null)
-// document.write(`Welcome ${userName}`);
+alert("Welcome to my site");
 
-// // Q4
+do {
+    var userName = prompt("Enter your name:");
+} while (!isNaN(userName) || userName === null);
+document.write(`Welcome ${userName}`);
 
-// var sum = 0;
-// do {
-//     var num = prompt(`Your total Sum is: ${sum}
-//                     \nEnter a Number:`);
+// Q4
 
-//     // if the variable num is of type "number"
-//     if (isFinite(num)) {
-//         sum += Number(num);
-//     };
+var sum = 0;
+do {
+    var num = prompt(`Your total sum is: ${sum}
+                    \nEnter a number:`);
 
-// } while(sum <= 100 || Number(num) === 0)
+    // if the variable num is of type "number"
+    if (isFinite(num)) {
+        sum += Number(num);
+    }
+} while (sum <= 100 && Number(num) !== 0);
 
-// // Q5
+// Q5
 
-// do {
-//     var message = prompt("Enter your message:");
-// } while(message === null)
+do {
+    var message = prompt("Enter your message:");
+} while (message === null);
 
-// for (var i = 1; i <= 6; i++) {
-//     document.write(`<h${i}>` + message + `</h${i}>`);
-// };
+for (var i = 1; i <= 6; i++) {
+    document.write(`<h${i}>` + message + `</h${i}>`);
+}
 
 // Q6
 
 do {
-    var userName = prompt("Your Name:");
-} while(!isNaN(userName) || userName === null)
+    var userName = prompt("Your name:");
+} while (!isNaN(userName) || userName === null);
 
 do {
-    var birthYear = Number(prompt("Your Birthyear:"));
-} while(!birthYear || birthYear > 2010)
+    var birthYear = Number(prompt("Your birth year:"));
+} while (!birthYear || birthYear > 2010);
 
 // while(!isFinite(birthYear) || birthYear == null || birthYear == "" || birthYear > 2010)
-// while(isNan(birthYear) || birthYear == null || birthYear == "" || birthYear > 2010)
+// while(isNaN(birthYear) || birthYear == null || birthYear == "" || birthYear > 2010)
 // while(!birthYear || birthYear > 2010)
 
-var date = new Date;
+var date = new Date();
 var currentYear = date.getFullYear();
 var age = currentYear - birthYear;
 
